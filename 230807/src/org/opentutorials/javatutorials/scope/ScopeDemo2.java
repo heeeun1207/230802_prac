118a05d997a6d254551fb3020ba34a776a3b1dd6
jungheeeun <jungheeeun272@gmail.com>
2023-08-07 17:39:20 +0900

    다양한 유효범위들

---
+++ 230807/src/org/opentutorials/javatutorials/scope/ScopeDemo2.java
@@ -0,0 +1,34 @@
+//package org.opentutorials.javatutorials.scope;
+// 
+//public class ScopeDemo2 {
+//    static int i;
+//     
+//    static void a() {
+//        i = 0;
+//    }
+// 
+//    public static void main(String[] args) {
+//        for (i = 0; i < 5; i++) {
+//            a();
+//            System.out.println(i);
+//        }
+//    }
+// 
+//}
+package org.opentutorials.javatutorials.scope;
+ 
+public class ScopeDemo2 {
+    static int i;
+     
+    static void a() {
+        int i = 0;
+    }
+ 
+    public static void main(String[] args) {
+        for (i = 0; i < 5; i++) {
+            a();
+            System.out.println(i);
+        }
+    }
+ 
+}
