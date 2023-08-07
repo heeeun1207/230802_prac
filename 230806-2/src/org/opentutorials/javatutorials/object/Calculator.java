fe1d0adb4f4560d1e507f0258e84279135e0c99f
jungheeeun <jungheeeun272@gmail.com>
2023-08-07 14:05:20 +0900

    클래스와 인스턴스 그리고 객체 - 사용자정의 데이터타입?

---
+++ 230806-2/src/org/opentutorials/javatutorials/object/Calculator.java
@@ -0,0 +1,38 @@
+package org.opentutorials.javatutorials.object;
+ 
+class Calculator{
+    int left, right;
+      
+    public void setOprands(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+      
+    public void sum(){
+        System.out.println(this.left+this.right);
+    }
+      
+    public void avg(){
+        System.out.println((this.left+this.right)/2);
+    }
+}
+  
+public class CalculatorDemo4 {
+      
+    public static void main(String[] args) {
+          
+        Calculator c1 = new Calculator();
+        //객체를 새로 만든 것.
+        //Calculator 객체를 새로만들어서 c1 변수에 담았다 . 
+        //그 변수는 Calculator 객체를 담을 수 있는 데이터 형식을 가지고 있어야한다.
+        c1.setOprands(10, 20);
+        c1.sum();       
+        c1.avg();       
+          
+        Calculator c2 = new Calculator();
+        c2.setOprands(20, 40);
+        c2.sum();       
+        c2.avg();
+    }
+  
+}
