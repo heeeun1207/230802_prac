64e5120b6526bd61ca3d51673a5139c5f7a8b94d
jungheeeun <jungheeeun272@gmail.com>
2023-08-08 15:51:04 +0900

    오버라이딩의 조건

--- 230807/src/org/opentutorials/javatutorials/overriding/example1/CalculatorDemo.java
+++ 230807/src/org/opentutorials/javatutorials/overriding/example1/CalculatorDemo.java
@@ -1,3 +1,49 @@
+//package org.opentutorials.javatutorials.overriding.example1;
+// 
+//class Calculator {
+//    int left, right;
+// 
+//    public void setOprands(int left, int right) {
+//        this.left = left;
+//        this.right = right;
+//    }
+// 
+//    public void sum() {
+//        System.out.println(this.left + this.right);
+//    }
+// 
+//    public int avg() {
+//        return ((this.left + this.right) / 2);
+//    }
+//}
+// 
+//class SubstractionableCalculator extends Calculator {
+//     
+//    public void sum() {
+//        System.out.println("실행 결과는 " +(this.left + this.right)+"입니다.");
+//    }
+//     
+//    public int avg() {
+//        return ((this.left + this.right) / 2);
+//    }
+//     
+//    public void substract() {
+//        System.out.println(this.left - this.right);
+//    }
+//}
+// 
+//public class CalculatorDemo {
+//    public static void main(String[] args) {
+//        SubstractionableCalculator c1 = new SubstractionableCalculator();
+//        c1.setOprands(10, 20);
+//        c1.sum();
+//        c1.avg();
+//        c1.substract();
+//    }
+//}
+
+
+//코드의 중복(public int avg() {})은 제거되야 하므로,   super를 사용해서 문제를 해결해보자. 
 package org.opentutorials.javatutorials.overriding.example1;
  
 class Calculator {
@@ -12,8 +58,8 @@ class Calculator {
         System.out.println(this.left + this.right);
     }
  
-    public void avg() {
-        System.out.println((this.left + this.right) / 2);
+    public int avg() {
+        return ((this.left + this.right) / 2);
     }
 }
  
@@ -21,6 +67,10 @@ class SubstractionableCalculator extends Calculator {
      
     public void sum() {
         System.out.println("실행 결과는 " +(this.left + this.right)+"입니다.");
+    }
+     
+    public int avg() {
+        return super.avg();
     }
      
     public void substract() {
@@ -33,7 +83,7 @@ public class CalculatorDemo {
         SubstractionableCalculator c1 = new SubstractionableCalculator();
         c1.setOprands(10, 20);
         c1.sum();
-        c1.avg();
+        System.out.println("실행 결과는" + c1.avg());
         c1.substract();
     }
 }
