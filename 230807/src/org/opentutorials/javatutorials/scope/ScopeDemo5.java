0f6592ed3de3f0358d9cbf69a6e458b12de2983e
jungheeeun <jungheeeun272@gmail.com>
2023-08-10 12:21:32 +0900

    메소드 오버로딩(overloading)  : 이름은 같지만 시그니처는 다른 메소드를 중복으로 선언 할 수 있는 방법

--- 230807/src/org/opentutorials/javatutorials/scope/ScopeDemo5.java
+++ 230807/src/org/opentutorials/javatutorials/scope/ScopeDemo5.java
@@ -5,7 +5,7 @@ public class ScopeDemo5 {
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
         }
-        // System.out.println(i);
+         System.out.println(i);
     }
  
 }
---
+++ 230810/src/org/opentutorials/javatutorials/overloading/example1/CalculatorDemo.java
@@ -0,0 +1,46 @@
+package org.opentutorials.javatutorials.overloading.example1;
+ 
+class Calculator{
+    int left, right;
+    int third = 0;
+      
+    public void setOprands(int left, int right){
+        System.out.println("setOprands(int left, int right)");
+        this.left = left;
+        this.right = right;
+    }
+     
+    public void setOprands(int left, int right, int third){
+        System.out.println("setOprands(int left, int right, int third)");
+        this.left = left;
+        this.right = right;
+        this.third = third;
+//setOprands가 두번 정의되었다. 이름이 충돌하고 있지만 문제가 없는것이 오버로딩 개념이다.
+//같은 이름이지만 다른 매개변수의 숫자나, 데이터타입이 다르다면 다른 메소드로 인식한다.
+//메소드 오버로딩  
+    }
+     
+    public void sum(){
+        System.out.println(this.left+this.right+this.third);
+    }
+      
+    public void avg(){
+        System.out.println((this.left+this.right+this.third)/3);
+    }
+}
+  
+public class CalculatorDemo {
+      
+    public static void main(String[] args) {
+          
+        Calculator c1 = new Calculator();
+        c1.setOprands(10, 20);
+        c1.sum();       
+        c1.avg();
+        c1.setOprands(10, 20, 30);
+        c1.sum();       
+        c1.avg();
+         
+    }
+  
+}
