b473e6dd2557a2561ae1018cbc3c0d7677aab589
jungheeeun <jungheeeun272@gmail.com>
2023-08-10 20:35:07 +0900

    while,for,for-each

---
+++ 230810/src/org/opentutorials/javatutorials/overloading/example1/loopPrac01.java
@@ -0,0 +1,24 @@
+package org.opentutorials.javatutorials.overloading.example1;
+
+public class loopPrac01 {
+//	   public static void main(String[] args) {
+//	        for (int i = 0; i < 10; i++) {
+//	            System.out.println("Coding Everybody " + i);
+//	        }
+//	 
+//	    }
+	
+//	   public static void main(String[] args) {
+//	int i = 0;
+//	while(i<10){
+//	    System.out.println("coding everybody"+i);
+//	    i++;
+//		}
+//	  }
+	   public static void main(String[] args) {
+	        String[] members = { "최진혁", "최유빈", "한이람" };
+	        for (String e : members) {
+	            System.out.println(e + "이 상담을 받았습니다");
+	        }
+	    }
+	}
