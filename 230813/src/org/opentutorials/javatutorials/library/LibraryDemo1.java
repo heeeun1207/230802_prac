da3116b93c1c289165cf8b4f3b13e4243c1382cf
jungheeeun <jungheeeun272@gmail.com>
2023-08-13 19:46:28 +0900

    import java.lang.*;

---
+++ 230813/src/org/opentutorials/javatutorials/library/LibraryDemo1.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.library;
+
+//import java.lang.*;
+//패키지 java.lang은 자바 프로그래밍을 하기 위해서 필수적인 클래스들을 모아둔 패키지이다.
+//클래스 System은 바로 이 java.lang의 소속이다.
+public class LibraryDemo1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
