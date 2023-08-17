931355259fee58f08fc15cc29c6be072a6acb04d
jungheeeun <jungheeeun272@gmail.com>
2023-08-17 12:24:14 +0900

    예외처리

---
+++ 230817/src/org/opentutorials/javatutorials/exception/ThrowExceptionDemo.java
@@ -0,0 +1,31 @@
+package org.opentutorials.javatutorials.exception;
+import java.io.*;
+class B{
+    void run(){
+        BufferedReader bReader = null;
+        String input = null;
+        try {
+            bReader = new BufferedReader(new FileReader("out.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try{
+            input = bReader.readLine();
+        } catch (IOException e){
+            e.printStackTrace();
+        }       
+        System.out.println(input); 
+    }
+}
+class C{
+    void run(){
+        B b = new B();
+        b.run();
+    }
+}
+public class ThrowExceptionDemo {
+    public static void main(String[] args) {
+         C c = new C();
+         c.run();
+    }   
+}
