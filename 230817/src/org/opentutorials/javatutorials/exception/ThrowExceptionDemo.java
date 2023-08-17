4e2468ddb47c55c76b02e2406261dc0f9aead83d
jungheeeun <jungheeeun272@gmail.com>
2023-08-17 12:25:03 +0900

    예외관리

--- 230817/src/org/opentutorials/javatutorials/exception/ThrowExceptionDemo.java
+++ 230817/src/org/opentutorials/javatutorials/exception/ThrowExceptionDemo.java
@@ -1,24 +1,16 @@
 package org.opentutorials.javatutorials.exception;
 import java.io.*;
 class B{
-    void run(){
+    void run() throws IOException, FileNotFoundException{
         BufferedReader bReader = null;
         String input = null;
-        try {
-            bReader = new BufferedReader(new FileReader("out.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        try{
-            input = bReader.readLine();
-        } catch (IOException e){
-            e.printStackTrace();
-        }       
-        System.out.println(input); 
+        bReader = new BufferedReader(new FileReader("out.txt"));
+        input = bReader.readLine();
+        System.out.println(input);
     }
 }
 class C{
-    void run(){
+    void run() throws IOException, FileNotFoundException{
         B b = new B();
         b.run();
     }
@@ -26,6 +18,13 @@ class C{
 public class ThrowExceptionDemo {
     public static void main(String[] args) {
          C c = new C();
-         c.run();
+         try {
+            c.run();
+        } catch (FileNotFoundException e) {
+            System.out.println("out.txt 파일은 설정 파일 입니다. 이 파일이 프로잭트 루트 디렉토리에 존재해야 합니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }   
 }
+//out.txt 파일은 설정 파일 입니다. 이 파일이 프로잭트 루트 디렉토리에 존재해야 합니다.
