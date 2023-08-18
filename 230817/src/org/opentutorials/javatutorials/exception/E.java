84dd7443af5dee6baa908f2f3c88689890840e8d
jungheeeun <jungheeeun272@gmail.com>
2023-08-18 10:09:02 +0900

    연습

--- 230817/src/org/opentutorials/javatutorials/exception/E.java
+++ 230817/src/org/opentutorials/javatutorials/exception/E.java
@@ -4,14 +4,10 @@ class E{
     void throwArithmeticException(){
         throw new ArithmeticException();
     }
-    void throwIOException1(){
-        try {
-            throw new IOException();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    void throwIOException2() throws IOException{
+    void throwIOException() throws IOException{
         throw new IOException();
     }
 }
+//IOException은 Exception의 자식이지만 RuntimeException의 자식은 아니다.
+//IOException 기본 개념을 이해해야 
+//내가 unchecked exception을 만들지 checked exception을 만들지 구분할 수 있게된다.
